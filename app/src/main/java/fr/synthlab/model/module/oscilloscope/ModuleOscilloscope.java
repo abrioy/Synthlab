d04e26e2d6e686895a906625d7b0b0e3467bf879
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-01 11:45:30 +0100

    Initialize oscilloscope

---
+++ app/src/main/java/fr/synthlab/model/module/oscilloscope/ModuleOscilloscope.java
@@ -0,0 +1,19 @@
+package fr.synthlab.model.module.oscilloscope;
+
+import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.Port;
+
+import java.util.Collection;
+
+/**
+ * Created by acobac on 01/02/16.
+ */
+public class ModuleOscilloscope implements Module {
+
+
+
+    @Override
+    public Collection<Port> getPorts() {
+        return null;
+    }
+}
