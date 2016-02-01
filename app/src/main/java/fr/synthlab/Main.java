b9ecb4c3a4e5e80d6ca014a1730bd6dd6622631b
Miow <abrioy@gmail.com>
2016-02-01 11:40:01 +0100

    Added log4j logger

--- app/src/main/java/fr/synthlab/Main.java
+++ app/src/main/java/fr/synthlab/Main.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 /**
  * Created by miow on 2/1/16.
  */
 public class Main extends Application {
+	private static final Logger logger = Logger.getLogger(Main.class);
 
 	@Override
 	public void start(Stage stage) throws Exception {
