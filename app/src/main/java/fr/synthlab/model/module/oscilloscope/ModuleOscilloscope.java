67e9d4d49b0e85131d9fd629933d6899137b2829
Corentin Beaucé <corentin.beauce@free.fr>
2016-02-02 10:17:20 +0100

    SCOP: Separated the View and Model more clearly

--- app/src/main/java/fr/synthlab/model/module/oscilloscope/ModuleOscilloscope.java
+++ app/src/main/java/fr/synthlab/model/module/oscilloscope/ModuleOscilloscope.java
@@ -1,12 +1,16 @@
 package fr.synthlab.model.module.oscilloscope;
 
+import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
 import com.jsyn.scope.AudioScope;
+import com.jsyn.unitgen.*;
 import fr.synthlab.model.module.Module;
 import fr.synthlab.model.module.port.InputPort;
 import org.apache.log4j.Logger;
 import fr.synthlab.model.module.port.Port;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -34,5 +38,71 @@ public class ModuleOscilloscope implements Module {
     @Override
     public void update() {
 
+    }
+
+    public JComponent getOscillatorJComponent() {
+        return new JOscillatorComponent(scope);
+    }
+
+    class JOscillatorComponent extends JComponent
+    {
+        private static final long serialVersionUID = -8315903842197137926L;
+        private ArrayList<UnitOscillator> oscillators = new ArrayList<UnitOscillator>();
+        private LineOut lineOut;
+        private Synthesizer synth;
+        private JPanel oscPanel;
+        private Multiply oscGain;
+        private LinearRamp freqRamp;
+
+        public JOscillatorComponent(AudioScope scope){
+            synth = scope.getModel().getSynthesizer();
+
+            // Use a multiplier for gain control and so we can hook up to the scope
+            // from a single unit.
+            synth.add(oscGain = new Multiply());
+            oscGain.inputB.setup(0.02, 0.5, 1.0);
+            oscGain.inputB.setName("Amplitude");
+
+            synth.add(freqRamp = new LinearRamp());
+            freqRamp.input.setup(50.0, 300.0, 20000.0);
+            freqRamp.input.setName("Frequency");
+            freqRamp.time.set(0.1);
+
+            // Add an output so we can hear the oscillators.
+            synth.add(lineOut = new LineOut());
+
+            oscGain.output.connect(lineOut.input);
+
+            setLayout( new BorderLayout() );
+
+            scope = new AudioScope( synth );
+            scope.addProbe( oscGain.output );
+            scope.setTriggerMode( AudioScope.TriggerMode.NORMAL );
+            scope.getView().setShowControls(false);
+            scope.getView().setBorder(BorderFactory.createLineBorder(Color.RED));
+            scope.start();
+            add(BorderLayout.CENTER, scope.getView());
+
+            oscPanel = new JPanel();
+            oscPanel.setLayout(new GridLayout(2, 5));
+
+            oscPanel.validate();
+            validate();
+
+            // Create a square oscillator and connects it
+            UnitOscillator osc = new TriangleOscillator();
+            synth.add(osc);
+            freqRamp.output.connect(osc.frequency);
+            osc.amplitude.set(1.0);
+            oscGain.inputA.disconnectAll(0);
+            osc.output.connect(oscGain.inputA);
+
+            // Start lineOut so it can pull data from other units.
+            lineOut.start();
+
+            // We only need to start the LineOut. It will pull data from the
+            // oscillator.
+            lineOut.start();
+        }
     }
 }
--- app/src/main/java/fr/synthlab/view/OscillatorPane.java
+++ app/src/main/java/fr/synthlab/view/OscillatorPane.java
@@ -1,16 +1,6 @@
 package fr.synthlab.view;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.util.ArrayList;
-
-import javax.swing.*;
-
-import com.jsyn.JSyn;
-import com.jsyn.Synthesizer;
-import com.jsyn.scope.AudioScope;
-import com.jsyn.unitgen.*;
-
+import fr.synthlab.model.module.oscilloscope.ModuleOscilloscope;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.layout.Pane;
 
@@ -19,103 +9,12 @@ import javafx.scene.layout.Pane;
  */
 public class OscillatorPane extends Pane {
     SwingNode swingNode;
+	ModuleOscilloscope osc;
 
-    public OscillatorPane() {
+    public OscillatorPane(ModuleOscilloscope osc) {
 		super();
 		swingNode = new SwingNode();
-		JOscillatorComponent osc = new JOscillatorComponent();
-		osc.start();
-        swingNode.setContent(osc);
+        //swingNode.setContent(osc.getOscillatorJComponent());
         this.getChildren().add(swingNode);
 	}
 }
-
-class JOscillatorComponent extends JComponent
-{
-	private static final long serialVersionUID = -8315903842197137926L;
-	private Synthesizer synth;
-	private ArrayList<UnitOscillator> oscillators = new ArrayList<UnitOscillator>();
-	private LineOut lineOut;
-	private AudioScope scope;
-	private JPanel oscPanel;
-	private Multiply oscGain;
-	private ButtonGroup buttonGroup;
-	private LinearRamp freqRamp;
-
-	public JOscillatorComponent()
-	{
-		/*
-		setSize(640, 500);
-		setVisible(true);
-		validate();
-		*/
-	}
-
-	private void setupGUI()
-	{
-		setLayout( new BorderLayout() );
-
-		scope = new AudioScope( synth );
-		scope.addProbe( oscGain.output );
-		scope.setTriggerMode( AudioScope.TriggerMode.NORMAL );
-		//scope.getModel().getTriggerModel().getLevelModel().setDoubleValue( 0.0001 );
-		// Turn off the gain and trigger control GUI.
-		scope.getView().setShowControls(false );
-		scope.start();
-		add(BorderLayout.CENTER, scope.getView());
-
-		oscPanel = new JPanel();
-		oscPanel.setLayout(new GridLayout(2, 5));
-
-		oscPanel.validate();
-		validate();
-	}
-
-	public void start()
-	{
-		synth = JSyn.createSynthesizer();
-
-		// Use a multiplier for gain control and so we can hook up to the scope
-		// from a single unit.
-		synth.add(oscGain = new Multiply());
-		oscGain.inputB.setup(0.02, 0.5, 1.0);
-		oscGain.inputB.setName("Amplitude");
-
-		synth.add(freqRamp = new LinearRamp());
-		freqRamp.input.setup(50.0, 300.0, 20000.0);
-		freqRamp.input.setName("Frequency");
-		freqRamp.time.set(0.1);
-
-		// Add an output so we can hear the oscillators.
-		synth.add(lineOut = new LineOut());
-
-		oscGain.output.connect(lineOut.input);
-
-		setupGUI();
-
-		// Create a square oscillator and connects it
-		UnitOscillator osc = new TriangleOscillator();
-		synth.add( osc );
-		freqRamp.output.connect( osc.frequency );
-		osc.amplitude.set( 1.0 );
-		oscGain.inputA.disconnectAll(0);
-		osc.output.connect(oscGain.inputA);
-
-		// Start synthesizer using default stereo output at 44100 Hz.
-		synth.start();
-		// Start lineOut so it can pull data from other units.
-		lineOut.start();
-
-		// We only need to start the LineOut. It will pull data from the
-		// oscillator.
-		lineOut.start();
-	}
-
-	public void stop()
-	{
-		scope.stop();
-		synth.stop();
-	}
-
-}
-
