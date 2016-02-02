af947c50a13e01dad2eef4b48e81a3a1b4433abd
pollt <pol.letue@gmail.com>
2016-02-02 14:52:19 +0100

    Changing name for simpler class into MainWindows

---
+++ app/src/main/java/fr/synthlab/view/controller/MenuBarController.java
@@ -0,0 +1,19 @@
+package fr.synthlab.view.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
+import org.apache.log4j.Logger;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuBarController implements Initializable {
+	private static final Logger logger = Logger.getLogger(MenuBarController.class);
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
--- app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
+++ app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
@@ -1,13 +1,17 @@
 package fr.synthlab.view.module;
 
+import javafx.fxml.FXML;
 import org.apache.log4j.Logger;
 
 public class ViewModuleVCO extends ViewModuleFrame {
 	private static final Logger logger = Logger.getLogger(ViewModuleVCO.class);
 
+
+
 	public ViewModuleVCO() {
 		super();
 		this.loadFXML("/gui/fxml/module/ViewModuleVCO.fxml");
+
 	}
 
 }
