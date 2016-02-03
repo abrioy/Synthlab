e6cfd428d320a7471f8693a2666413de87fcd0a1
snadour <salim.nadour@gmail.com>
2016-02-03 11:46:21 +0100

    Javadoc VCOA et port

--- app/src/main/java/fr/synthlab/model/module/port/InputPort.java
+++ app/src/main/java/fr/synthlab/model/module/port/InputPort.java
@@ -5,9 +5,15 @@ import fr.synthlab.model.module.Module;
 
 import java.util.logging.Logger;
 
+/**
+ *
+ */
 public class InputPort extends Port {
 	private static final Logger logger = Logger.getLogger(InputPort.class.getName());
 
+    /**
+     * The matching port in JSyn
+     */
     private ConnectableInput input;
 
     public InputPort(String name, Module m, ConnectableInput input) {
--- app/src/main/java/fr/synthlab/model/module/vcoa/ModuleVCOA.java
+++ app/src/main/java/fr/synthlab/model/module/vcoa/ModuleVCOA.java
@@ -14,28 +14,71 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
 
+/**
+ * VCO module ( Voltage Control Oscillator ) for generating a periodic
+ * signal whose shape can be selected and the frequency can be controlled by another signal
+ */
 public class ModuleVCOA implements Module {
 	private static final Logger logger = Logger.getLogger(ModuleVCOA.class.getName());
 
+    /**
+     * list of port of the VCO module
+     */
     private Collection<Port> ports = new ArrayList<>();
 
+    /**
+     * The frequency f0 of the VCO
+     */
     private double frequency = 450;
     private double octave = 0;
     private double tone = 0;
 
+    /**
+     * Filter modulator
+     */
     private VcoFm fmFilter = new VcoFm(frequency);
 
-    // Oscillators
+    /**
+     * Square oscillator
+     */
     private SquareOscillator squareOscillator = new SquareOscillator();
+
+    /**
+     * Triangle Oscillator
+     */
     private TriangleOscillator triangleOscillator = new TriangleOscillator();
+
+    /**
+     * Sawtooth Oscillator
+     */
     private SawtoothOscillator sawtoothOscillator = new SawtoothOscillator();
 
-    // Ports
+    /**
+     * Filter modulator input port
+     */
     private InputPort fmInput;
+
+    /**
+     * Square oscillator output port
+     */
     private OutputPort squareOutput;
+
+    /**
+     * Triangle oscillator output port
+     */
     private OutputPort triangleOutput;
+
+    /**
+     * Sawtooth oscillator output port
+     */
     private OutputPort sawtoothOutput;
 
+
+    /**
+     * Constructor
+     *
+     * @param synthesizer
+     */
     public ModuleVCOA(Synthesizer synthesizer) {
         synthesizer.add(squareOscillator);
         synthesizer.add(triangleOscillator);
@@ -52,14 +95,22 @@ public class ModuleVCOA implements Module {
         ports.add(triangleOutput);
         ports.add(sawtoothOutput);
 
+        // Initialize the frequency of the fm filter and the 3 oscillators
         setFrequency(frequency);
     }
 
+    /**
+     *
+     * @return the list of port of the VCO
+     */
     @Override
     public Collection<Port> getPorts() {
         return ports;
     }
 
+    /**
+     * Start the VCO
+     */
     @Override
     public void start() {
         fmFilter.start();
@@ -68,6 +119,9 @@ public class ModuleVCOA implements Module {
         triangleOscillator.start();
     }
 
+    /**
+     * Stop the VCO
+     */
     @Override
     public void stop() {
         fmFilter.stop();
@@ -76,13 +130,20 @@ public class ModuleVCOA implements Module {
         triangleOscillator.stop();
     }
 
+    /**
+     *
+     * @return the frequency f0 of the VCO
+     */
     public double getFrequency() {
         return frequency;
     }
 
+    /**
+     * set the frequency of the VCO
+     * @param frequency
+     */
     public void setFrequency(double frequency) {
         this.frequency = frequency;
-        fmFilter.setf0(frequency * Math.pow(2, octave));
 
         if (fmInput.getConnected() == null) {
             squareOscillator.frequency.set(frequency * Math.pow(2, octave));
@@ -91,24 +152,46 @@ public class ModuleVCOA implements Module {
         }
     }
 
+    /**
+     *
+     * @return the octave of VCO
+     */
     public double getOctave() {
         return octave;
     }
 
+    /**
+     * set octave of VCO
+     * @param octave
+     */
     public void setOctave(double octave) {
         this.octave = octave;
         setFrequency(frequency);
     }
 
-
+    /**
+     *
+     * @return
+     */
     public double getTone() {
         return tone;
     }
 
+    /**
+     *
+     * @param tone
+     */
     public void setTone(double tone) {
         this.tone = tone;
     }
 
+    /**
+     * This method is called by the input port fm of the VCO when its state has changed
+     * When nothing is connected to the input port of fm, the 3 oscillators has the same frequency f0
+     * When something is connected to the input port of fm, we connect the output port of fm filter
+     * to input port of each oscillator
+     *
+     */
     @Override
     public void update() {
         if (fmInput.getConnected() == null) {
