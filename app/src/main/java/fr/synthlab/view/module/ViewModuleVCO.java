8c9cd2db8af551f8a823030e33d14b13b7ca3cf9
J <nomail@mail.com>
2016-02-04 14:53:02 +0100

    Add members in ViewModuleVCO

--- app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
+++ app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
@@ -1,9 +1,28 @@
 package fr.synthlab.view.module;
 
+import fr.synthlab.view.component.Knob;
+import fr.synthlab.view.component.Plug;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
-public class ViewModuleVCO extends ViewModule {
+public class ViewModuleVCO extends ViewModule implements Initializable {
 	private static final Logger logger = Logger.getLogger(ViewModuleVCO.class.getName());
+
+	@FXML
+	private Knob freq;
+	@FXML
+	private Knob freqLine;
+	@FXML
+	private Knob picker;
+	@FXML
+	private Plug in;
+	@FXML
+	private Plug out;
 
 	public ViewModuleVCO() {
 		super();
@@ -11,4 +30,12 @@ public class ViewModuleVCO extends ViewModule {
 		this.setId("pane");
 	}
 
+	private void OnMouseDragOverFreq(MouseEvent event) {
+		System.out.println(event.getX());
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		//freq.setOnMouseMoved(event -> OnMouseDragOverFreq(event));
+	}
 }
