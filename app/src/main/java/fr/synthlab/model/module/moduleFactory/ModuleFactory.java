4e16e547057f37d87a788621ab6073d9ec7209d4
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-04 11:20:38 +0100

    create enum type for modules

--- app/src/main/java/fr/synthlab/model/module/moduleFactory/ModuleFactory.java
+++ app/src/main/java/fr/synthlab/model/module/moduleFactory/ModuleFactory.java
@@ -2,6 +2,8 @@ package fr.synthlab.model.module.moduleFactory;
 
 import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
+import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.ModuleEnum;
 import fr.synthlab.model.module.oscilloscope.ModuleOscilloscope;
 import fr.synthlab.model.module.out.ModuleOut;
 import fr.synthlab.model.module.vcoa.ModuleVCOA;
@@ -13,24 +15,36 @@ public class ModuleFactory {
      */
     private static Synthesizer syn = JSyn.createSynthesizer();
 
+    public static Module createModule(ModuleEnum module){
+        Module m;
+        switch(module){
+            case VCOA: m = createVCO();
+                break;
+            case SCOP: m = createOscilloscope();
+                break;
+            default : m = createOut(); //OUT
+        }
+        return m;
+    }
+
     /**
      * @return a new VCO
      */
-    public static ModuleVCOA createVCO() {
+    private static ModuleVCOA createVCO() {
         return new ModuleVCOA(syn);
     }
 
     /**
      * @return a new Oscilloscope
      */
-    public static ModuleOscilloscope createOscilloscope() {
+    private static ModuleOscilloscope createOscilloscope() {
         return new ModuleOscilloscope(syn);
     }
 
     /**
      * @return a new ModuleOut
      */
-    public static ModuleOut createOut() {
+    private static ModuleOut createOut() {
         return new ModuleOut(syn);
     }
 
