ef4ead934c15754cfbc7b316ebef1dae6b63c128
snadour <salim.nadour@gmail.com>
2016-02-04 11:23:44 +0100

    add ViewModuleFactory

--- app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
+++ app/src/main/java/fr/synthlab/view/module/ViewModuleVCO.java
@@ -1,14 +1,45 @@
 package fr.synthlab.view.module;
 
+import fr.synthlab.model.Command;
+
 import java.util.logging.Logger;
 
 public class ViewModuleVCO extends ViewModule {
 	private static final Logger logger = Logger.getLogger(ViewModuleVCO.class.getName());
 
+	// Commands
+	private Command changeShape;
+	private Command changeFreq;
+
+	private double freq;
+	private double freqFin;
+
+
+
 	public ViewModuleVCO() {
 		super();
 		this.loadFXML("/gui/fxml/module/ViewModuleVCO.fxml");
 		this.setId("pane");
+	}
+
+
+	public void setChangeShape(Command changeShape) {
+		this.changeShape = changeShape;
+	}
+
+
+	public void setChangeFreq(Command changeFreq) {
+		this.changeFreq = changeFreq;
+	}
+
+
+	public double getFreq() {
+		return freq;
+	}
+
+
+	public double getFreqFin() {
+		return freqFin;
 	}
 
 }
--- app/src/main/java/fr/synthlab/view/viewModuleFactory/ViewModuleFactory.java
+++ app/src/main/java/fr/synthlab/view/viewModuleFactory/ViewModuleFactory.java
@@ -1,5 +1,28 @@
 package fr.synthlab.view.viewModuleFactory;
 
 
+import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.moduleFactory.ModuleFactory;
+import fr.synthlab.model.module.vcoa.ModuleVCOA;
+import fr.synthlab.view.module.ViewModuleVCO;
+
 public class ViewModuleFactory {
+
+    /**
+     * @return a viewModuleVCO attached to its module
+     */
+    public static ViewModuleVCO creatViewModuleVCO() {
+        Module vco = ModuleFactory.createVCO();
+        ViewModuleVCO viewVco = new ViewModuleVCO();
+        viewVco.setModule(vco);
+
+        viewVco.setChangeFreq(() -> {
+            ((ModuleVCOA) vco).setFrequency(viewVco.getFreq() + viewVco.getFreqFin());
+        });
+
+
+        return viewVco;
+    }
+
+
 }
