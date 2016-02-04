864517e83936afcde7d21d31e6d2e6ec35e9dedf
snadour <salim.nadour@gmail.com>
2016-02-04 12:00:29 +0100

    add ShapeEnum and viewModuleFactory

--- app/src/main/java/fr/synthlab/view/viewModuleFactory/ViewModuleFactory.java
+++ app/src/main/java/fr/synthlab/view/viewModuleFactory/ViewModuleFactory.java
@@ -2,26 +2,55 @@ package fr.synthlab.view.viewModuleFactory;
 
 
 import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.ModuleEnum;
 import fr.synthlab.model.module.moduleFactory.ModuleFactory;
 import fr.synthlab.model.module.vcoa.ModuleVCOA;
+import fr.synthlab.view.module.ViewModule;
 import fr.synthlab.view.module.ViewModuleVCO;
 
 public class ViewModuleFactory {
 
+
+    public static ViewModule creatViewModule(ModuleEnum m) {
+        switch (m) {
+            case VCOA:
+                return creatViewModuleVCO();
+            case OUT:
+                return creatViewModuleOut();
+            case SCOP:
+                return creatViewModuleScop();
+        }
+        return null;
+    }
+
+
+
+
     /**
      * @return a viewModuleVCO attached to its module
      */
-    public static ViewModuleVCO creatViewModuleVCO() {
-        Module vco = ModuleFactory.createVCO();
+    private static ViewModuleVCO creatViewModuleVCO() {
+        Module vco = ModuleFactory.createModule(ModuleEnum.VCOA);
         ViewModuleVCO viewVco = new ViewModuleVCO();
         viewVco.setModule(vco);
 
-        viewVco.setChangeFreq(() -> {
+        viewVco.setChangeFreqCommand(() -> {
             ((ModuleVCOA) vco).setFrequency(viewVco.getFreq() + viewVco.getFreqFin());
         });
 
+        viewVco.setChangeShapeCommand(() -> {
+            ((ModuleVCOA) vco).setShape(viewVco.getSelectedShape());
+        });
 
         return viewVco;
+    }
+
+    private static ViewModule creatViewModuleOut() {
+        return null;
+    }
+
+    private static ViewModule creatViewModuleScop() {
+        return null;
     }
 
 
