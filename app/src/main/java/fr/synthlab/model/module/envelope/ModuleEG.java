886561829fbafdc35f89af77c8e204d45aa4e303
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-08 15:38:06 +0100

    Add setter.

--- app/src/main/java/fr/synthlab/model/module/envelope/ModuleEG.java
+++ app/src/main/java/fr/synthlab/model/module/envelope/ModuleEG.java
@@ -1,18 +1,36 @@
 package fr.synthlab.model.module.envelope;
 
+import com.jsyn.unitgen.EnvelopeDAHDSR;
 import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.port.InputPort;
+import fr.synthlab.model.module.port.OutputPort;
 import fr.synthlab.model.module.port.Port;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ModuleEG implements Module {
 
+    /**
+     * All ports
+     */
+    private ArrayList<Port> ports = new ArrayList<>();
+
+    EnvelopeDAHDSR envelope;
+
     public ModuleEG() {
+        envelope = new EnvelopeDAHDSR();
+
+        InputPort in = new InputPort("in", this, envelope.input);
+        ports.add(in);
+        OutputPort out = new OutputPort("out", this, envelope.output);
+        ports.add(out);
+
     }
 
     @Override
     public Collection<Port> getPorts() {
-        return null;
+        return ports;
     }
 
     @Override
@@ -32,6 +50,22 @@ public class ModuleEG implements Module {
 
     @Override
     public String getName() {
-        return null;
+        return "EG ADSR";
+    }
+
+    public void setAttack(double attack){
+
+    }
+
+    public void setDecay(double decay){
+
+    }
+
+    public void setSustain(double sustain){
+
+    }
+
+    public void setRelease(double release){
+
     }
 }
