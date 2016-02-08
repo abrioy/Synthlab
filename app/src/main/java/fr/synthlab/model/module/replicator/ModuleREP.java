c05d6a1aad400b0613bed720805239ec554334b3
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-08 14:41:07 +0100

    Complete rep module

--- app/src/main/java/fr/synthlab/model/module/replicator/ModuleREP.java
+++ app/src/main/java/fr/synthlab/model/module/replicator/ModuleREP.java
@@ -1,21 +1,32 @@
 package fr.synthlab.model.module.replicator;
 
+import com.jsyn.unitgen.PassThrough;
 import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.port.InputPort;
+import fr.synthlab.model.module.port.OutputPort;
 import fr.synthlab.model.module.port.Port;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-/**
- * Created by acobac on 08/02/16.
- */
 public class ModuleREP implements Module {
 
+
+    private ArrayList<Port> ports = new ArrayList<>();
+
     public ModuleREP() {
+
+        PassThrough pt = new PassThrough();
+
+        InputPort in = new InputPort("in", this, pt.input);
+        OutputPort out1 = new OutputPort("out1", this, pt.output);
+        OutputPort out2 = new OutputPort("out2", this, pt.output);
+        OutputPort out3 = new OutputPort("out3", this, pt.output);
     }
 
     @Override
     public Collection<Port> getPorts() {
-        return null;
+        return ports;
     }
 
     @Override
