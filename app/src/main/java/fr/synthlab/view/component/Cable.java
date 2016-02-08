ca9aae49905ce1ae8c8f6a65fa21ad75a37f6bb5
pollt <pol.letue@gmail.com>
2016-02-08 17:05:30 +0100

    beautiful color like the rainbow in the sky with poptarts still bug with front display (cable and plug going in the background sometime)

--- app/src/main/java/fr/synthlab/view/component/Cable.java
+++ app/src/main/java/fr/synthlab/view/component/Cable.java
@@ -6,18 +6,23 @@ import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurve;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.StrokeLineCap;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
  * Created by pollt on 2/2/16.
  */
 
-public class Cable extends Line {
+public class Cable extends CubicCurve {
     private static final Logger logger = Logger.getLogger(Cable.class.getName());
-
+    private Color color;
     private Plug in;
     private Plug out;
 
@@ -27,9 +32,15 @@ public class Cable extends Line {
     private Workbench workbench;
 
     public Cable(Workbench workbench, Plug in) {
+        List<Color> colors= new ArrayList<>();
+        colors.add(Color.DARKBLUE);
+        colors.add(Color.DARKGREEN);
+        colors.add(Color.DARKGOLDENROD);
+        colors.add(Color.DARKRED);
+        colors.add(Color.DARKTURQUOISE);
+
         this.in = in;
         this.workbench = workbench;
-
         circleIn = new Circle();
         circleOut = new Circle();
         circleIn.setMouseTransparent(true);
@@ -38,11 +49,10 @@ public class Cable extends Line {
         circleOut.setFill(Color.DARKGRAY);
         workbench.getChildren().add(circleIn);
         workbench.getChildren().add(circleOut);
-
+        color=getRandomColor(colors);
         this.setStrokeWidth(10);
         this.setStrokeLineCap(StrokeLineCap.ROUND);
         this.setMouseTransparent(true);
-
     }
 
     public void update(){
@@ -56,17 +66,14 @@ public class Cable extends Line {
         this.setEndX(outPosition.getX());
         this.setEndY(outPosition.getY());
 
-
+        drawCable(inPosition, outPosition);
         addCircle(circleIn, inPosition.getX(), inPosition.getY());
         addCircle(circleOut, outPosition.getX(), outPosition.getY());
-        this.setFill(Color.RED);
 
         this.toFront();
     }
 
     public void update(Point2D mouse){
-        //in=getPlug();
-        //out=null;
         Bounds inBounds = getPlug().getBoundsInLocal();
         Point2D inPosition = workbench.getBoundsCenter(workbench.sceneToLocal(getPlug().localToScene(inBounds)));
 
@@ -75,9 +82,10 @@ public class Cable extends Line {
         this.setEndX(mouse.getX());
         this.setEndY(mouse.getY());
 
+        drawCable(inPosition,mouse);
         addCircle(circleIn, inPosition.getX(), inPosition.getY());
         addCircle(circleOut,mouse.getX(),mouse.getY());
-        this.setFill(Color.RED);
+
 
         this.toFront();
     }
@@ -130,5 +138,21 @@ public class Cable extends Line {
         workbench.getChildren().remove(circleIn);
         workbench.getChildren().remove(circleOut);
 
+    }
+    private void drawCable(Point2D start, Point2D end){
+        double diffX=start.getX() - end.getX();
+        double diffY=Math.abs(start.getY() - end.getY());
+        logger.info("DIFFERENCE : " + diffY);
+        this.setControlX1(start.getX() - diffX / 3);
+        this.setControlY1(start.getY()+100+diffY/2);
+        this.setControlX2(end.getX() + diffX / 3);
+        this.setControlY2(end.getY()+100+diffY/2);
+        this.setFill(null);
+        this.setStroke(color);
+    }
+    private Color getRandomColor(List<Color> colors){
+        Random rnd = new Random();
+        int i = rnd.nextInt(colors.size());
+        return (Color) colors.toArray()[i];
     }
 }
