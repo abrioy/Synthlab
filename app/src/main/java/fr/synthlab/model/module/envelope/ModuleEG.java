402d60411b65340ebd34e19cf17e7566036e0248
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-08 15:22:13 +0100

    Initialize moduleEG

---
+++ app/src/main/java/fr/synthlab/model/module/envelope/ModuleEG.java
@@ -0,0 +1,37 @@
+package fr.synthlab.model.module.envelope;
+
+import fr.synthlab.model.module.Module;
+import fr.synthlab.model.module.port.Port;
+
+import java.util.Collection;
+
+public class ModuleEG implements Module {
+
+    public ModuleEG() {
+    }
+
+    @Override
+    public Collection<Port> getPorts() {
+        return null;
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
