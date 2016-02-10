53b498ed25c3497fa8aa2f3ab895ddd4f8eb734c
Miow <abrioy@gmail.com>
2016-02-10 13:51:34 +0100

    Small tweaks to the drag and drop to make it feel more natural

--- app/src/main/java/fr/synthlab/view/controller/MainWindowController.java
+++ app/src/main/java/fr/synthlab/view/controller/MainWindowController.java
@@ -111,7 +111,6 @@ public class MainWindowController implements Initializable {
 		workbench.setOnDragOver(event -> {
 			if (draggedNewViewModule != null) {
 				Point2D localPoint = workbench.sceneToLocal(new Point2D(event.getSceneX(), event.getSceneY()));
-				workbench.moveGhost(localPoint.getX(), localPoint.getY());
 
 				Point2D newLocation = workbench.computeNewModulePosition(draggedNewViewModule, localPoint.getX(), localPoint.getY());
 				if(newLocation != null){
