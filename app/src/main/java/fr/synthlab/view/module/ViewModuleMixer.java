462a868615ce2e9bf1b0d96362b01fc9eac2e77f
championj <johanchampion@hotmail.fr>
2016-02-10 15:22:28 +0100

    #Mix add action on knob

--- app/src/main/java/fr/synthlab/view/module/ViewModuleMixer.java
+++ app/src/main/java/fr/synthlab/view/module/ViewModuleMixer.java
@@ -1,6 +1,9 @@
 package fr.synthlab.view.module;
 
+import fr.synthlab.model.Command;
 import fr.synthlab.view.Workbench;
+import fr.synthlab.view.component.Knob;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -10,21 +13,74 @@ import java.util.logging.Logger;
 public class ViewModuleMixer extends ViewModule implements Initializable{
     private static final Logger logger = Logger.getLogger(ViewModuleMixer.class.getName());
 
+    @FXML
+    private Knob attenuator1;
+
+    @FXML
+    private Knob attenuator2;
+
+    @FXML
+    private Knob attenuator3;
+
+    public double getAttenuator4() {
+        return attenuator4.getValue();
+    }
+
+    public double getAttenuator1() {
+        return attenuator1.getValue();
+    }
+
+    public double getAttenuator2() {
+        return attenuator2.getValue();
+    }
+
+    public double getAttenuator3() {
+        return attenuator3.getValue();
+    }
+
+    @FXML
+    private Knob attenuator4;
+
+    public void setAttenuator1Cmd(Command attenuator1Cmd) {
+        this.attenuator1Cmd = attenuator1Cmd;
+    }
+
+    public void setAttenuator2Cmd(Command attenuator2Cmd) {
+        this.attenuator2Cmd = attenuator2Cmd;
+    }
+
+    public void setAttenuator3Cmd(Command attenuator3Cmd) {
+        this.attenuator3Cmd = attenuator3Cmd;
+    }
+
+    public void setAttenuator4Cmd(Command attenuator4Cmd) {
+        this.attenuator4Cmd = attenuator4Cmd;
+    }
+
+    private Command attenuator1Cmd;
+    private Command attenuator2Cmd;
+    private Command attenuator3Cmd;
+    private Command attenuator4Cmd;
+
     public ViewModuleMixer(Workbench workbench) {
         super(workbench);
         this.loadFXML("/gui/fxml/module/ViewModuleMixer.fxml");
         this.setId("pane");
-        //todo add listener on mute
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        /*picker.valueProperty().addListener(event -> {
-            volume.execute();
+        attenuator1.valueProperty().addListener(event -> {
+            attenuator1Cmd.execute();
         });
-        muteButton.setOnAction(event -> {
-            mute = !mute;
-            muteCommand.execute();
-        });*/
+        attenuator2.valueProperty().addListener(event -> {
+            attenuator2Cmd.execute();
+        });
+        attenuator3.valueProperty().addListener(event -> {
+            attenuator3Cmd.execute();
+        });
+        attenuator4.valueProperty().addListener(event -> {
+            attenuator4Cmd.execute();
+        });
     }
 }
