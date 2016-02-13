0db519fe98af8a81aa59bb13935232f539e71339
Anthony Cobac <anthony.cobac@gmail.com>
2016-02-13 10:57:55 +0100

    Add test case for factory

--- app/src/test/fr/synthlab/model/filter/KeyboardFilterTest.java
+++ app/src/test/fr/synthlab/model/filter/KeyboardFilterTest.java
@@ -15,30 +15,30 @@ public class KeyboardFilterTest extends TestCase {
     }
 
     public void testInitializeTension() throws Exception {
-        assertEquals(0, keyboardFilter.getTension());
+        assertEquals(0.0, keyboardFilter.getTension());
     }
 
     public void testPressKey() throws Exception {
         keyboardFilter.pressKey();
-        assertEquals(5, keyboardFilter.getTension());
+        assertEquals(5.0, keyboardFilter.getTension());
     }
 
     public void testPressKey2() throws Exception {
         keyboardFilter.pressKey();
         keyboardFilter.releaseKey();
         keyboardFilter.pressKey();
-        assertEquals(5, keyboardFilter.getTension());
+        assertEquals(5.0, keyboardFilter.getTension());
     }
 
     public void testReleaseKey() throws Exception {
         keyboardFilter.releaseKey();
-        assertEquals(-5, keyboardFilter.getTension());
+        assertEquals(-5.0, keyboardFilter.getTension());
     }
 
     public void testReleaseKey2() throws Exception {
         keyboardFilter.pressKey();
         keyboardFilter.releaseKey();
-        assertEquals(-5, keyboardFilter.getTension());
+        assertEquals(-5.0, keyboardFilter.getTension());
     }
 
     public void testGetGate() throws Exception {
