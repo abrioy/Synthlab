7743d96a91147d0f261ba8198285208fad3a1b1c
Miow <abrioy@gmail.com>
2016-03-02 14:26:58 +0100

    Reimplemented the cables

--- app/src/main/java/fr/synthlab/view/component/Cable.java
+++ app/src/main/java/fr/synthlab/view/component/Cable.java
@@ -1,7 +1,8 @@
 package fr.synthlab.view.component;
 
-import fr.synthlab.view.controller.Workbench;
+import fr.synthlab.model.module.port.Port;
 import fr.synthlab.view.controller.ToolboxController;
+import fr.synthlab.view.controller.Workbench;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -19,15 +20,14 @@ public class Cable extends CubicCurve {
             = Logger.getLogger(Cable.class.getName());
     private final double circleRadius = 12.0d;
     private Color color;
-    private Plug in;
-    private Plug out;
-    private Circle circleIn;
-    private Circle circleOut;
+    private Plug plug1;
+    private Plug plug2;
+    private Circle circle1;
+    private Circle circle2;
     private Workbench workbench;
 
     public Cable(final Workbench workbenchInit, final Plug inInit) {
-        in = inInit;
-        in.setCable(this);
+		connectPlug(inInit);
         workbench = workbenchInit;
 
         init();
@@ -35,24 +35,24 @@ public class Cable extends CubicCurve {
 
     public Cable(final Workbench workbenchInit,
                  final Plug inInit, final Plug outInit) {
-        in = inInit;
-        in.setCable(this);
-        out = outInit;
-        out.setCable(this);
+		connectPlug(inInit);
+		connectPlug(outInit);
         workbench = workbenchInit;
 
         init();
     }
 
     private void init() {
-        circleIn = new Circle();
-        circleOut = new Circle();
-        circleIn.setMouseTransparent(true);
-        circleOut.setMouseTransparent(true);
-        circleIn.setFill(Color.DARKGRAY);
-        circleOut.setFill(Color.DARKGRAY);
-        workbench.getChildren().add(circleIn);
-        workbench.getChildren().add(circleOut);
+        circle1 = new Circle();
+        circle2 = new Circle();
+        circle1.setMouseTransparent(true);
+        circle2.setMouseTransparent(true);
+        circle1.setFill(Color.DARKGRAY);
+        circle2.setFill(Color.DARKGRAY);
+		circle1.setRadius(circleRadius);
+		circle2.setRadius(circleRadius);
+        workbench.getChildren().add(circle1);
+        workbench.getChildren().add(circle2);
         color = ToolboxController.getColor();
 
         this.setStrokeWidth(10);
@@ -60,27 +60,90 @@ public class Cable extends CubicCurve {
         this.setMouseTransparent(true);
     }
 
-    public final void updateCircles() {
-        circleIn.toFront();
-        circleOut.toFront();
-    }
+	private void connectPorts(Port port1, Port port2){
+		if(port1 != null && port2 != null) {
+			port1.connect(port2);
+		}
+		else{
+			LOGGER.severe("Trying to connect a null port.");
+		}
+	}
+
+
+	public void connectPlug(Plug plug){
+		if(plug1 == null){
+			plug1 = plug;
+			plug1.setCable(this);
+			if(plug2 != null){
+				connectPorts(plug1.getPort(), plug2.getPort());
+			}
+		}
+		else if(plug2 == null){
+			plug2 = plug;
+			plug2.setCable(this);
+			if(plug1 != null){
+				connectPorts(plug1.getPort(), plug2.getPort());
+			}
+		}
+		else{
+			LOGGER.severe("Trying to connect a cable to more than 2 plugs.");
+		}
+	}
+
+	public final void disconnectPlug(final Plug plug) {
+		if (plug1 == plug) {
+			plug1.setCable(null);
+			if(plug1.getPort() != null){
+				plug1.getPort().disconnect();
+			}
+			plug1 = null;
+		}
+		else if (plug2 == plug) {
+			plug2.setCable(null);
+			if(plug2.getPort() != null){
+				plug2.getPort().disconnect();
+			}
+			plug2 = null;
+		}
+		else{
+			LOGGER.severe("Trying to disconnect a cable from a plug it is not connected to.");
+		}
+	}
+
+	public final Plug getConnectedPlug() {
+		if (plug1 != null && plug2 == null) {
+			return plug1;
+		}
+		else if (plug1 == null && plug2 != null) {
+			return plug2;
+		}
+		else {
+			LOGGER.warning("Trying to get information on a connected plug when there is 0 or 2 connected plugs.");
+			return null;
+		}
+	}
 
     public final void update() {
-        Point2D inPosition = workbench.sceneToLocal(
-                in.localToScene(in.getCenter()));
-        Point2D outPosition = workbench.sceneToLocal(
-                out.localToScene(out.getCenter()));
+		if(plug1 != null && plug2 != null) {
+			Point2D position1 = workbench.sceneToLocal(
+					plug1.localToScene(plug1.getCenter()));
+			Point2D position2 = workbench.sceneToLocal(
+					plug2.localToScene(plug2.getCenter()));
 
-        this.setStartX(inPosition.getX());
-        this.setStartY(inPosition.getY());
-        this.setEndX(outPosition.getX());
-        this.setEndY(outPosition.getY());
+			drawCable(position1, position2);
 
-        drawCable(inPosition, outPosition);
-        addCircle(circleIn, inPosition.getX(), inPosition.getY());
-        addCircle(circleOut, outPosition.getX(), outPosition.getY());
+			this.toFront();
+		}
+		else {
+			Plug plug = getConnectedPlug();
+			if (plug != null){
+				Point2D plugPosition = workbench.sceneToLocal(
+						plug.localToScene(plug.getCenter()));
 
-        this.toFront();
+				drawCable(plugPosition, plugPosition);
+				this.toFront();
+			}
+		}
     }
 
     public final void update(final Point2D mouse) {
@@ -88,87 +151,69 @@ public class Cable extends CubicCurve {
                 circleRadius, mouse.getX()),
                 Math.max(circleRadius, mouse.getY()));
 
-        in = getPluggedPlug();
-        if (out != null) {
-            out.setCable(null);
-            out = null;
-        }
-        Point2D inPosition = workbench.sceneToLocal(
-                in.localToScene(in.getCenter()));
+        Plug plug = getConnectedPlug();
+		if (plug != null) {
+			Point2D plugPosition = workbench.sceneToLocal(
+					plug.localToScene(plug.getCenter()));
 
-        this.setStartX(inPosition.getX());
-        this.setStartY(inPosition.getY());
-        this.setEndX(correctedMouse.getX());
-        this.setEndY(correctedMouse.getY());
+			drawCable(plugPosition, correctedMouse);
 
-        drawCable(inPosition, mouse);
-        addCircle(circleIn, inPosition.getX(), inPosition.getY());
-        addCircle(circleOut, correctedMouse.getX(), correctedMouse.getY());
-
-        this.toFront();
+			circle2.toFront();
+			this.toFront();
+		}
     }
+
+	public final void updateCircles() {
+		circle1.toFront();
+		circle2.toFront();
+	}
 
     public final Plug getOppositePlug(final Plug plug) {
-        if (in == null || out == null) {
+        if (plug1 == null || plug2 == null) {
             return null;
         }
-        if (in.equals(plug)) {
-            return out;
-        } else if (out.equals(plug)) {
-            return in;
+        if (plug1.equals(plug)) {
+            return plug2;
+        } else if (plug2.equals(plug)) {
+            return plug1;
         } else {
             return null;
         }
     }
 
-    public final Plug getPluggedPlug() {
-        if (in == null) {
-            return out;
-        } else {
-            return in;
-        }
-    }
-
-    public final void setEmptyPlug(final Plug plug) {
-        if (in == null) {
-            in = plug;
-            in.setCable(this);
-        } else {
-            out = plug;
-            out.setCable(this);
-        }
-    }
-
-    private void addCircle(final Circle c, final double x, final double y) {
+    private void moveCircle(final Circle c, final double x, final double y) {
         c.setCenterX(x);
         c.setCenterY(y);
-        c.setRadius(circleRadius);
     }
 
-    public final void allToFront() {
-        circleIn.toFront();
-        circleOut.toFront();
-        this.toFront();
-    }
+    public final void dispose() {
+		if(plug1 != null) {
+			plug1.setCable(null);
+			if (plug1.getPort() != null) {
+				plug1.getPort().disconnect();
+			}
+		}
 
-    public final void unplug(final Plug plug) {
-        if (in == plug) {
-            in.setCable(null);
-            in = null;
-        } else {
-            if (out != null) {
-                out.setCable(null);
-                out = null;
-            }
-        }
-    }
+		if(plug2 != null) {
+			plug2.setCable(null);
+			if (plug2.getPort() != null) {
+				plug2.getPort().disconnect();
+			}
+		}
 
-    public final void deleteCircles() {
-        workbench.getChildren().remove(circleIn);
-        workbench.getChildren().remove(circleOut);
+        workbench.getChildren().remove(circle1);
+        workbench.getChildren().remove(circle2);
     }
 
     private void drawCable(final Point2D start, final Point2D end) {
+		this.setStartX(start.getX());
+		this.setStartY(start.getY());
+		this.setEndX(end.getX());
+		this.setEndY(end.getY());
+
+		moveCircle(circle1, start.getX(), start.getY());
+		moveCircle(circle2, end.getX(), end.getY());
+
         double diffX = start.getX() - end.getX();
         double diffY = Math.abs(start.getY() - end.getY());
         this.setControlX1(start.getX() - diffX / 3);
