df7b04a32558333a89fc83f1a87caea83c7d988a
Anthony Cobac <anthony.cobac@gmail.com>
2016-03-06 12:28:25 +0100

    Add test on two filters

--- app/src/test/fr/synthlab/model/module/replicator/FilterREPTest.java
+++ app/src/test/fr/synthlab/model/module/replicator/FilterREPTest.java
@@ -3,12 +3,15 @@ package fr.synthlab.model.module.replicator;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
  * Test on the replicator filter.
  */
 public class FilterREPTest {
+
+
 
     /**
      * Filter tested.
@@ -53,5 +56,31 @@ public class FilterREPTest {
     @Test
     public void testGetOut3() {
         assertNotNull(filterREP.getOut3());
+    }
+
+    /**
+     * Test on generate.
+     */
+    @Test
+    public void testGenerate() {
+        filterREP.generate(0,1);
+
+        assertEquals(0, filterREP.getOut1().getValue(), 0.00000001);
+        assertEquals(0, filterREP.getOut2().getValue(), 0.00000001);
+        assertEquals(0, filterREP.getOut3().getValue(), 0.00000001);
+    }
+
+    /**
+     * Test on generate.
+     */
+    @Test
+    public void testGenerate2() {
+        filterREP.getIn().set(100.0);
+
+        filterREP.generate(0,1);
+
+        assertEquals(100.0, filterREP.getOut1().getValue(), 0.00000001);
+        assertEquals(100.0, filterREP.getOut2().getValue(), 0.00000001);
+        assertEquals(100.0, filterREP.getOut3().getValue(), 0.00000001);
     }
 }
--- app/src/test/fr/synthlab/model/module/sequencer/SEQFilterTest.java
+++ app/src/test/fr/synthlab/model/module/sequencer/SEQFilterTest.java
@@ -2,16 +2,20 @@ package fr.synthlab.model.module.sequencer;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 
 /**
  * Tests on the sequencer filter
  */
+@RunWith(MockitoJUnitRunner.class)
 public class SEQFilterTest {
 
     /**
@@ -61,5 +65,27 @@ public class SEQFilterTest {
     {
         seqFilter.reset();
         assertEquals(0, seqFilter.getCurrent());
+    }
+
+    /**
+     * test on generate.
+     */
+    @Test
+    public void testGenerate() {
+        seqFilter.generate(0,1);
+
+        assertEquals(0, seqFilter.getOutput().getValue(), 0.0000001);
+    }
+
+    /**
+     * test on generate.
+     */
+    @Test
+    public void testGenerate2() {
+        seqFilter.getInput().set(10);
+        seqFilter.generate(0,1);
+
+        verify(mockModuleSEQ).updateObs();
+        assertEquals(0, seqFilter.getOutput().getValue(), 0.0000001);
     }
 }
