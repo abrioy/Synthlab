50d11eff546d75af93e2d4f5f22120a51f60a7cb
championj <johanchampion@hotmail.fr>
2016-03-09 19:14:41 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/model/module/ModuleType.java
+++ app/src/main/java/fr/synthlab/model/module/ModuleType.java
@@ -4,29 +4,84 @@ package fr.synthlab.model.module;
  * Singleton for different module type.
  */
 public enum ModuleType {
+    /**
+     * VCOA.
+     */
     VCOA("VCOA", "VCO Type A"),
+    /**
+     * VCA.
+     */
     VCA("VCA", "VCA"),
+    /**
+     * Out.
+     */
     OUT("OUT", "Audio Output"),
+    /**
+     * Scop.
+     */
     SCOP("SCOP", "Oscilloscope"),
+    /**
+     * Rep.
+     */
     REP("REP", "Repeater"),
+    /**
+     * EG.
+     */
     EG("EG", "Envelope Generator"),
+    /**
+     * VCFLP.
+     */
     VCFLP("VCFLP", "VCF (LP)"),
+    /**
+     * VCFHP.
+     */
     VCFHP("VCFHP", "VCF (HP)"),
+    /**
+     * Keyboard.
+     */
     KEYB("KEYB", "Keyboard"),
+    /**
+     * mix.
+     */
     MIX("MIX", "Mixer"),
+    /**
+     * white noise.
+     */
     BRUI("BRUI", "White Noise"),
+    /**
+     * Sequencer.
+     */
     SEQ("SEQ", "Sequencer");
 
+    /**
+     * serial.
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * name of module.
+     */
     private String moduleName;
+    /**
+     * complete name of module.
+     */
     private String moduleLongName;
 
+    /**
+     * constructor.
+     * @param name of module
+     * @param longName complete name
+     */
     ModuleType(final String name, final String longName) {
         moduleName = name;
         moduleLongName = longName;
     }
 
+    /**
+     * getter by long name.
+     * @param longName module to found
+     * @return module type
+     */
     public static String getNameFromLong(final String longName) {
         for (ModuleType m : values()) {
             if (m.getLongName().equals(longName)) {
@@ -36,10 +91,17 @@ public enum ModuleType {
         return "";
     }
 
+    /**
+     * @return the complete name
+     */
     public String getLongName() {
         return this.moduleLongName;
     }
 
+    /**
+     * transform in string.
+     * @return it is the name of module
+     */
     @Override
     public String toString() {
         return moduleName;
--- app/src/main/java/fr/synthlab/model/module/keyboard/NoteKEYB.java
+++ app/src/main/java/fr/synthlab/model/module/keyboard/NoteKEYB.java
@@ -5,30 +5,85 @@ package fr.synthlab.model.module.keyboard;
  * @see NoteKEYB
  */
 public enum NoteKEYB {
+    /**
+     * C.
+     */
     C(-9),
+    /**
+     * CSharp.
+     */
     CSharp(-8),
+    /**
+     * D.
+     */
     D(-7),
+    /**
+     * DSharp.
+     */
     DSharp(-6),
+    /**
+     * E.
+     */
     E(-5),
+    /**
+     * F.
+     */
     F(-4),
+    /**
+     * FSharp.
+     */
     FSharp(-3),
+    /**
+     * G.
+     */
     G(-2),
+    /**
+     * GSharp.
+     */
     GSharp(-1),
+    /**
+     * A.
+     */
     A(0),
+    /**
+     * ASharp.
+     */
     ASharp(1),
+    /**
+     * B.
+     */
     B(2),
+    /**
+     * C2.
+     */
     C2(3);
 
+    /**
+     * value of note.
+     */
     private final int value;
 
+    /**
+     * constructor.
+     * @param newValue value of note
+     */
     NoteKEYB(final int newValue) {
         value = newValue;
     }
 
+    /**
+     * getter value.
+     * @return value of note
+     */
     public int getValue() {
-        return this.value;
+        return value;
     }
 
+    /**
+     * getter on note by value.
+     * @param val search
+     * @return note
+     */
     public static NoteKEYB fromValue(final int val) {
         for (NoteKEYB k : NoteKEYB.values()) {
             if (k.getValue() == val) {
