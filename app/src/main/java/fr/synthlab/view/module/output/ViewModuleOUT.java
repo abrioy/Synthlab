4daf0ac05506b1d317e865ce945e20653ff20609
championj <johanchampion@hotmail.fr>
2016-03-09 20:19:29 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/module/output/ViewModuleOUT.java
+++ app/src/main/java/fr/synthlab/view/module/output/ViewModuleOUT.java
@@ -15,33 +15,72 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * Created by miow on 3/1/16.
+ * view module out.
  */
 public interface ViewModuleOUT
         extends EventTarget, Styleable, Serializable, Initializable {
+    /**
+     * @return knob for volume
+     */
     Knob getPicker();
 
+    /**
+     * setter command on volume change.
+     * @param newVolume execute on change
+     */
     void setVolumeCommand(Runnable newVolume);
 
+    /**
+     * setter command on mute change.
+     * @param mute execute on change
+     */
     void setMuteCommand(Runnable mute);
 
+    /**
+     * @return if is mute
+     */
     boolean isMute();
 
+    /**
+     * setter command on record change.
+     * @param record execute on change
+     */
     void setRecordCommand(Runnable record);
 
+    /**
+     * @return if is recording
+     */
     boolean isRecording();
 
+    /**
+     * setter on record.
+     * @param value to set
+     */
     void setIsRecording(boolean value);
 
+    /**
+     * @return directory file for audio save
+     */
     File getRecordingFile();
 
     @Override
     void initialize(
             URL location, ResourceBundle resources);
 
+    /**
+     * write in o for save workbench.
+     * @param o where is save
+     * @throws IOException if save can't open
+     */
     void writeObject(ObjectOutputStream o)
             throws IOException;
 
+    /**
+     * reload object.
+     * @param o where is reload
+     * @throws IOException if save can't open
+     * @throws ClassNotFoundException if a save class can't be found
+     */
     void readObject(ObjectInputStream o)
             throws IOException, ClassNotFoundException;
 }
