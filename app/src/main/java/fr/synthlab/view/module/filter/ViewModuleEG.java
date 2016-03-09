7f7bba6d055de8dc51592e3ac1248df20471a69d
championj <johanchampion@hotmail.fr>
2016-03-09 21:39:34 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/module/filter/ViewModuleEG.java
+++ app/src/main/java/fr/synthlab/view/module/filter/ViewModuleEG.java
@@ -12,7 +12,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * Created by miow on 3/1/16.
+ * view module EG.
  */
 public interface ViewModuleEG
         extends EventTarget, Styleable, Serializable, Initializable {
@@ -20,25 +20,64 @@ public interface ViewModuleEG
     void initialize(
             URL location, ResourceBundle resources);
 
+    /**
+     * @return attack
+     */
     double getAttack();
 
+    /**
+     * @return decay
+     */
     double getDecay();
 
+    /**
+     * @return sustain
+     */
     double getSustain();
 
+    /**
+     * @return release
+     */
     double getRelease();
 
+    /**
+     * setter on command execute on change attack.
+     * @param command to set
+     */
     void setChangeAttackCommand(Runnable command);
 
+    /**
+     * setter on command execute on change decay.
+     * @param command to set
+     */
     void setChangeDecayCommand(Runnable command);
 
+    /**
+     * setter on command execute on change sustain.
+     * @param command to set
+     */
     void setChangeSustainCommand(Runnable command);
 
+    /**
+     * setter on command execute on change release.
+     * @param command to set
+     */
     void setChangeReleaseCommand(Runnable command);
 
+    /**
+     * write in o for save workbench.
+     * @param o where is save
+     * @throws IOException if save can't open
+     */
     void writeObject(ObjectOutputStream o)
-                    throws IOException;
+            throws IOException;
 
+    /**
+     * reload object.
+     * @param o where is reload
+     * @throws IOException if save can't open
+     * @throws ClassNotFoundException if a save class can't be found
+     */
     void readObject(ObjectInputStream o)
                             throws IOException, ClassNotFoundException;
 }
