f255b80359d72b209b1140b6a21a133806d89522
championj <johanchampion@hotmail.fr>
2016-03-09 22:03:36 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/module/filter/ViewModuleSEQImpl.java
+++ app/src/main/java/fr/synthlab/view/module/filter/ViewModuleSEQImpl.java
@@ -18,52 +18,127 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.logging.Logger;
 
+/**
+ * Implementation of View module sequencer.
+ */
 public class ViewModuleSEQImpl extends ViewModule
         implements ViewModuleSEQ {
+    /**
+     * Logger.
+     */
     private static final Logger LOGGER
             = Logger.getLogger(ViewModuleSEQImpl.class.getName());
 
+    /**
+     * button reset.
+     */
     @FXML
     private ResetButton resetButton;
 
+    /**
+     * knob frequency step 1.
+     */
     @FXML
     private Knob step1Picker;
+
+    /**
+     * knob frequency step 2.
+     */
     @FXML
     private Knob step2Picker;
+
+    /**
+     * knob frequency step 3.
+     */
     @FXML
     private Knob step3Picker;
+
+    /**
+     * knob frequency step 4.
+     */
     @FXML
     private Knob step4Picker;
+
+    /**
+     * knob frequency step 5.
+     */
     @FXML
     private Knob step5Picker;
+
+    /**
+     * knob frequency step 6.
+     */
     @FXML
     private Knob step6Picker;
+
+    /**
+     * knob frequency step 7.
+     */
     @FXML
     private Knob step7Picker;
+
+    /**
+     * knob frequency step 8.
+     */
     @FXML
     private Knob step8Picker;
 
+    /**
+     * Label for step.
+     */
     @FXML
     private Label stepLabel;
 
+    /**
+     * List of knob.
+     */
     private List<Knob> stepPickers;
 
+    /**
+     * setter on command execute reset.
+     */
     private Runnable resetCommand;
+    /**
+     * setter on command execute on step come to 1.
+     */
     private Runnable step1Command;
+    /**
+     * setter on command execute on step come to 2.
+     */
     private Runnable step2Command;
+    /**
+     * setter on command execute on step come to 3.
+     */
     private Runnable step3Command;
+    /**
+     * setter on command execute on step come to 4.
+     */
     private Runnable step4Command;
+    /**
+     * setter on command execute on step come to 5.
+     */
     private Runnable step5Command;
+    /**
+     * setter on command execute on step come to 6.
+     */
     private Runnable step6Command;
+    /**
+     * setter on command execute on step come to 7.
+     */
     private Runnable step7Command;
+    /**
+     * setter on command execute on step come to 8.
+     */
     private Runnable step8Command;
 
-
+    /**
+     * constructor.
+     * @param workbench current workbench
+     */
     public ViewModuleSEQImpl(final Workbench workbench) {
         super(workbench);
         this.loadFXML("/gui/fxml/module/ViewModuleSEQ.fxml");
         this.setId("pane");
-        //resetButton.setPrefSize(30,30);
     }
 
     @Override
