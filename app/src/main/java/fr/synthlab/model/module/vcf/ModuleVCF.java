521e2ac961c31b3e30994628adc89b04c2125f96
championj <johanchampion@hotmail.fr>
2016-03-09 19:02:52 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/model/module/vcf/ModuleVCF.java
+++ app/src/main/java/fr/synthlab/model/module/vcf/ModuleVCF.java
@@ -1,6 +1,5 @@
 package fr.synthlab.model.module.vcf;
 
-
 import com.jsyn.Synthesizer;
 import fr.synthlab.model.filter.FilterFm;
 import fr.synthlab.model.module.Module;
@@ -12,13 +11,32 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
 
+/**
+ * module VCF.
+ */
 public abstract class ModuleVCF implements Module {
+    /**
+     * Logger.
+     */
     private static final Logger LOGGER
             = Logger.getLogger(ModuleVCF.class.getName());
 
+    /**
+     * List of ports.
+     */
     protected Collection<Port> ports = new ArrayList<>();
+
+    /**
+     * port fm.
+     */
     protected InputPort fmInput;
+    /**
+     * input port.
+     */
     protected InputPort input;
+    /**
+     * output port.
+     */
     protected OutputPort output;
     /**
      * Fréquence de coupure f0.
@@ -30,6 +48,10 @@ public abstract class ModuleVCF implements Module {
      */
     protected FilterFm filterFm;
 
+    /**
+     * Constructor.
+     * @param synthesizer jsyn
+     */
     public ModuleVCF(final Synthesizer synthesizer) {
         filterFm = new FilterFm(f0);
 
@@ -39,25 +61,36 @@ public abstract class ModuleVCF implements Module {
         ports.add(fmInput);
     }
 
+    @Override
     public final Collection<Port> getPorts() {
         return ports;
     }
 
+    @Override
     public void start() {
         filterFm.start();
     }
 
+    @Override
     public void stop() {
         filterFm.stop();
     }
 
+    @Override
     public void update() {
     }
 
+    /**
+     * @return current f0
+     */
     public final double getF0() {
         return f0;
     }
 
+    /**
+     * setter on f0.
+     * @param newF0 to set in f0
+     */
     public void setF0(final double newF0) {
         f0 = newF0;
         filterFm.setF0(f0);
