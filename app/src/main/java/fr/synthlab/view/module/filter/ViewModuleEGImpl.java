a0cc9f25f01da3dac3957a7cccfb9a4db7a76f71
championj <johanchampion@hotmail.fr>
2016-03-09 21:43:05 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/module/filter/ViewModuleEGImpl.java
+++ app/src/main/java/fr/synthlab/view/module/filter/ViewModuleEGImpl.java
@@ -12,25 +12,57 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
+/**
+ * Implementation of view module EG.
+ */
 public class ViewModuleEGImpl extends ViewModule implements ViewModuleEG {
+    /**
+     * Logger.
+     */
     private static final Logger LOGGER
             = Logger.getLogger(ViewModuleEGImpl.class.getName());
-
+    /**
+     * knob on attack.
+     */
     @FXML
     private Knob attack;
+    /**
+     * knob on decay.
+     */
     @FXML
     private Knob decay;
+    /**
+     * knob on sustain.
+     */
     @FXML
     private Knob sustain;
+    /**
+     * knob on release.
+     */
     @FXML
     private Knob release;
 
+    /**
+     * setter on command execute on change attack.
+     */
     private Runnable changeAttackCommand;
+    /**
+     * setter on command execute on change decay.
+     */
     private Runnable changeDecayCommand;
+    /**
+     * setter on command execute on change sustain.
+     */
     private Runnable changeSustainCommand;
+    /**
+     * setter on command execute on change release.
+     */
     private Runnable changeReleaseCommand;
 
-
+    /**
+     * constructor.
+     * @param workbench current workbench
+     */
     public ViewModuleEGImpl(final Workbench workbench) {
         super(workbench);
         this.loadFXML("/gui/fxml/module/ViewModuleEG.fxml");
