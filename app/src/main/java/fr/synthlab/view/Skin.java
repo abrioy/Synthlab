36453f1a4c029d1309215cd09ae93f850174495e
championj <johanchampion@hotmail.fr>
2016-03-09 22:48:26 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/Skin.java
+++ app/src/main/java/fr/synthlab/view/Skin.java
@@ -4,26 +4,59 @@ package fr.synthlab.view;
  * Enum for skin.
  */
 public enum Skin {
+    /**
+     * skin default.
+     */
     Default("Default"),
+    /**
+     * skin Black.
+     */
     Black("Black"),
+    /**
+     * skin Carbon.
+     */
     Carbon("Carbon"),
+    /**
+     * skin Aluminum.
+     */
     Aluminum("Aluminum"),
+    /**
+     * skin Wood.
+     */
     Wood("Wood"),
+    /**
+     * skin Colorful.
+     */
     Colorful("Colorful");
 
-
+    /**
+     * serial.
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * skin name.
+     */
     private String name;
 
+    /**
+     * constructor.
+     * @param newName skin name
+     */
     Skin(final String newName) {
         name = newName;
     }
 
+    /**
+     * @return skin name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return path to skin css
+     */
     public String getPath() {
         return "/gui/fxml/style/skin/" + this.name + ".css";
     }
