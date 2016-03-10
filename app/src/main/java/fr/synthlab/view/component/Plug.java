f2bcde0c9e7a2030e0918cc9e7b315e070b7d3ae
championj <johanchampion@hotmail.fr>
2016-03-10 20:12:52 +0100

    add some javadoc

--- app/src/main/java/fr/synthlab/view/component/Plug.java
+++ app/src/main/java/fr/synthlab/view/component/Plug.java
@@ -21,42 +21,91 @@ import java.util.logging.Logger;
  * draw plug.
  */
 public class Plug extends StackPane {
+    /**
+     * Logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(Plug.class.getName());
 
+    /**
+     * color plug.
+     */
     private Circle colorCircle;
+    /**
+     * cable connected to this plug.
+     */
     private Cable cable;
 
+    /**
+     * current workbench.
+     */
     private Workbench workbench;
 
+    /**
+     * setter on workbench.
+     * @param newWorkbench to set
+     */
     public final void setWorkbench(final Workbench newWorkbench) {
         this.workbench = newWorkbench;
     }
 
+    /**
+     * Callable port.
+     */
     private Callable<Port> getPortCommand = null;
 
+    /**
+     * setter on callable port.
+     * @param newGetPortCommand to set
+     */
     public final void setGetPortCommand(
             final Callable<Port> newGetPortCommand) {
         getPortCommand = newGetPortCommand;
     }
 
-
+    /**
+     * name of plug.
+     */
     private Label nameLabel;
 
     /**
      * type of plug.
      */
     public enum Type {
+        /**
+         * input plug.
+         */
         input(Color.LIGHTBLUE),
+        /**
+         * output plug.
+         */
         output(Color.ORANGE),
+        /**
+         * modulation plug.
+         */
         modulation(Color.HOTPINK),
+        /**
+         * other plug.
+         */
         other(Color.WHITE);
 
+        /**
+         * color of plug.
+         */
         private Color color;
 
+        /**
+         * constructor.
+         * @param aColor color
+         */
         Type(final Color aColor) {
             color = aColor;
         }
 
+        /**
+         * get type by its name.
+         * @param name of type
+         * @return type of plug
+         */
         public static Type getType(final String name) {
             switch (name) {
                 case "input":
@@ -71,18 +120,33 @@ public class Plug extends StackPane {
         }
     }
 
+    /**
+     * property on label.
+     */
     private final StringProperty label
             = new SimpleStringProperty(this, "label", "");
+    /**
+     * property on name in the model.
+     */
     private final StringProperty name
             = new SimpleStringProperty(this, "name", "");
+    /**
+     * property on type of plug.
+     */
     private final StringProperty type
             = new SimpleStringProperty(this, "type", "other");
 
+    /**
+     * constructor.
+     */
     public Plug() {
         super();
         init();
     }
 
+    /**
+     * initialize plug.
+     */
     private void init() {
         this.getStylesheets().add(
                 getClass().getResource("/gui/fxml/style/Plug.css")
@@ -117,11 +181,17 @@ public class Plug extends StackPane {
         });
     }
 
+    /**
+     * @return center of plug in view.
+     */
     public final Point2D getCenter() {
         return colorCircle.localToParent(
                 colorCircle.getCenterX(), colorCircle.getCenterY());
     }
 
+    /**
+     * @return port of plug in the model
+     */
     public final Port getPort() {
         if (getPortCommand != null) {
             try {
@@ -133,48 +203,85 @@ public class Plug extends StackPane {
         return null;
     }
 
+    /**
+     * setter on type of plug.
+     * @param v to set
+     */
     public final void setType(final String v) {
         type.set(v);
         colorCircle.setStroke(Type.getType(type.get()).color);
     }
 
+    /**
+     * @return type of plug
+     */
     public final String getType() {
         return type.get();
     }
 
+    /**
+     * @return type property
+     */
     public final StringProperty typeProperty() {
         return type;
     }
 
-
+    /**
+     * setter on name.
+     * @return name of plug
+     */
     public final String getName() {
         return name.get();
     }
 
+    /**
+     * @return property name
+     */
     public final StringProperty nameProperty() {
         return name;
     }
 
+    /**
+     * setter on name.
+     * @param newName to set
+     */
     public final void setName(final String newName) {
         name.set(newName);
     }
 
+    /**
+     * @return label
+     */
     public final String getLabel() {
         return label.get();
     }
 
+    /**
+     * @return label property
+     */
     public final StringProperty labelProperty() {
         return label;
     }
 
+    /**
+     * setter on label.
+     * @param newLabel to set
+     */
     public final void setLabel(final String newLabel) {
         label.set(newLabel);
     }
 
+    /**
+     * @return cable connected
+     */
     public final Cable getCable() {
         return cable;
     }
 
+    /**
+     * setter on cable.
+     * @param newCable connect
+     */
     public final void setCable(final Cable newCable) {
         cable = newCable;
     }
